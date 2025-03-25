3a49fe21696569a5cbd5803109d020d60cdae9c2
DanielaNava19 <anavaj@megacable.com.mx>
2025-03-25 17:20:07 -0600

    anavaj
    Adriana Daniela Nava Juarez
    Clase 2

--- Modulo_1_Javascript_Testing/02_sesion/Clase2.js
+++ Modulo_1_Javascript_Testing/02_sesion/Clase2.js
@@ -1,6 +1,18 @@
-var nombre= 'Daniela';
-var apellido= 'Nava';
-var nombreCompleto =`${nombre} ${apellido}`;
-var edad = 30;
+let intentos= 0;
+let contraseña= "12345";
+let contraseña_ingresada;
 
-console.log (`Hola soy: ${nombreCompleto} y tengo ${edad} años de edad`);
+while (intentos <3){
+    contraseña_ingresada=prompt("Ingresa la contraseña");
+    if (contraseña===contraseña_ingresada){
+        console.log ("La contraseña es correcta");
+        break;
+    }else {
+        console.log("Contraseña incorrecta");
+    }
+    intentos++;
+}
+
+if(intentos===3){
+    console.log("Sus intentos se han agotado");
+}
