11d4b4fb9a18de4ca22afbacb9abfefe1bb77331
DanielaNava19 <anavaj@megacable.com.mx>
2025-03-25 11:45:53 -0600

    anavaj
    Adriana Daniela Nava Juarez
    Clase 1

--- Modulo_1_Javascript_Testing/01_sesion/Clase1.js
+++ Modulo_1_Javascript_Testing/01_sesion/Clase1.js
@@ -1,6 +1,6 @@
 var nombre= 'Daniela';
 var apellido= 'Nava';
 var nombreCompleto =`${nombre} ${apellido}`;
-var edad = 31;
+var edad = 30;
 
 console.log (`Hola soy: ${nombreCompleto} y tengo ${edad} años de edad`);
