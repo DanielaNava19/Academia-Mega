cf1f06af24306f32b694e839e2cfa2bac6307e75
DanielaNava19 <anavaj@megacable.com.mx>
2025-04-06 10:15:38 -0600

    anavaj
    Adriana Daniela Nava Juárez
    Sesión 9

---
+++ Modulo_1_Javascript_Testing/09_sesion/main.js
@@ -0,0 +1,9 @@
+import {saludar, despedir, sumar, restar, multiplicar, dividir} from "./saludo.js"
+
+const nombre= "Daniela";
+console.log (saludar("Adriana"));
+console.log (despedir(nombre));
+console.log (sumar(5,5));
+console.log (restar(10,5));
+console.log (multiplicar(5,5));
+console.log (dividir(10,5));
---
+++ Modulo_1_Javascript_Testing/09_sesion/saludo.js
@@ -0,0 +1,23 @@
+export function saludar(nombre){
+    return `Hola, ${nombre}! Bienvenido a la clase de modulo de javascript`;
+}
+
+export function despedir(nombre){
+    return `Adios, ${nombre}! Nos vemos`;
+}
+
+export function sumar(num1, num2){
+    return num1+num2;
+}
+
+export function restar(num1, num2){
+    return num1-num2;
+}
+
+export function multiplicar(num1, num2){
+    return num1*num2;
+}
+
+export function dividir(num1, num2){
+    return num1/num2;
+}
