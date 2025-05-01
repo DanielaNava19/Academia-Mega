c66224f1b4178995f6bb43e7701ae608c66d46ca
DanielaNava19 <anavaj@megacable.com.mx>
2025-04-30 19:53:15 -0600

    anavaj
    Adriana Daniela Nava Juárez
    Proyecto tarjeta, utilizando los 4 tipos de Data Binding

---
+++ Modulo_2_Angular/03_sesion/src/app/Components/Saludo/saludo.components.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-saludo",
+    imports: [],
+    templateUrl: "./saludo.components.html",
+    styleUrl: "./saludo.components.css" ,
+
+})
+
+export class Saludo{
+    name = "Daniela Nava"
+}
