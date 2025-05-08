6b0ec9657572009c23eb10c70d96460207268415
DanielaNava19 <anavaj@megacable.com.mx>
2025-05-08 09:12:10 -0600

    anavaj
    Adriana Daniela Nava Juárez
    Sesión 12- integración de una api

---
+++ Modulo_2_Angular/12_sesion/src/app/Page/productos-info/productos-info.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ProductosComponent } from "../../Components/productos/productos.component";
+import { CommonModule } from '@angular/common';
+import { DetallesComponent } from '../../Components/detalles/detalles.component';
+
+@Component({
+  selector: 'app-productos-info',
+  imports: [ProductosComponent, CommonModule, DetallesComponent],
+  templateUrl: './productos-info.component.html',
+  styleUrl: './productos-info.component.css'
+})
+export class ProductosInfoComponent {
+items=[
+  {id: 1, nombre: "Producto A", descripcion: "Des del producto A", precio: 100},
+  {id: 2, nombre: "Producto B", descripcion: "Des del producto B", precio: 200},
+  {id: 3, nombre: "Producto C", descripcion: "Des del producto C", precio: 300},
+  {id: 4, nombre: "Producto D", descripcion: "Des del producto D", precio: 400},
+]
+itemSeleccionado: any= null;
+
+seleccionarItem(item: any){
+  this.itemSeleccionado = item;
+}
+
+
+
+}
